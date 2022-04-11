f49d9321c44ba77521e553868d906f970f5345fe
20194266-halt <ha.lt194266@sis.hust.edu.vn>
2022-04-11 14:05:28 +0700

    HelloConstraint final

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.helloconstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +13,15 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
-
+    private Button zeroButton;
+    private Button countButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        zeroButton = (Button) findViewById(R.id.button_zero);
+        countButton = (Button) findViewById(R.id.button_count);
     }
     public void showToast(View view){
         Toast toast = Toast.makeText(this, R.string.toast_message,
@@ -27,10 +32,15 @@ public class MainActivity extends AppCompatActivity {
     public void setZero(View view) {
         mCount = 0;
         mShowCount.setText(Integer.toString(mCount));
-
+        zeroButton.setBackgroundColor(Color.GRAY);
+        countButton.setBackgroundColor(Color.BLUE);
     }
     public void countUp(View view) {
         mCount++;
+        if(mCount>0){
+            zeroButton.setBackgroundColor(Color.RED);
+            countButton.setBackgroundColor(Color.GREEN);
+        }
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
